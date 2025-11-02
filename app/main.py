7645d1901dcbfe27200d85238df074da465558f5
Akansha Saraswat <akansha.saraswat3@gmail.com>
2025-11-02 18:12:44 +0530

    prod deployment

--- app/main.py
+++ app/main.py
@@ -34,4 +34,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message" : "Hello World! My First Production Deployment"}
+    return {"message" : "Hello World! My First Production Deployment and server"}
